a1595dc3dbd715057f20e5625f04ec28d951ffdf
MoWahidAlbadawi <mohammadwahidalbadawi@gmail.com>
2024-11-12 15:25:05 +0300

    il8next library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Container , Button , Text} from "@chakra-ui/react"
+import { Box, Container , Text} from "@chakra-ui/react"
 import { Route , Routes} from "react-router-dom"
 import MainPage from "./pages/MainPage"
 import AddGoods from "./pages/AddGoods"
@@ -16,14 +16,14 @@ function App() {
     <>
     <Navbar />
       <Container bg='gray.300' minHeight='90vh' p='20px'>
-      <Box textAlign={'right'} mt={{base : '-10px' , md : '-5px'}} mb={{base :  '15px' , md : '0px'}}>
+      <Box textAlign={'right'} mt={{base : '-10px' , md : '-5px'}} mb={{base :  '10px' , md : '0px'}}>
         <Text color={'blue.800'} mb={'5px'} mr={'10px'}>{t('selectLanguage')}</Text>
-    <Button onClick={() => changeLanguage('en')} colorPalette={'blue'} variant={'subtle'} padding={'10px'} mr={'5px'}>English</Button>
-    <Button onClick={() => changeLanguage('ar')} colorPalette={'blue'} variant={'subtle'} padding={'10px'}>العربية</Button>
+    <button onClick={() => changeLanguage('en')} className="button-language">English</button>
+    <button onClick={() => changeLanguage('ar')} className="button-language">العربية</button>
     </Box>
       <Routes>
         <Route path="/" element={<MainPage />}/>
-        <Route path="/addgoods" element={<AddGoods />}/>
+        <Route path="/addgoods" element={<AddGoods/>}/>
       </Routes>
     </Container>
     </>
