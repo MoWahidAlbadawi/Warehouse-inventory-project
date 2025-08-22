6531d82b29a87c247dc78b757c8d1c7531a3ef7b
MoWahidAlbadawi <mohammadwahidalbadawi@gmail.com>
2025-08-22 21:29:19 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
+// For Using vite enviroment keys in html 
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vitejs.dev/config/
+declare const process: {
+  env: Record<string, string | undefined>
+  cwd(): string
+}
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), 
+    tsconfigPaths(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          googleSiteVerification: process.env.VITE_GOOGLE_SITE_VERIFICATION
+        }
+      }
+    })
+  ],
 })
+
+
